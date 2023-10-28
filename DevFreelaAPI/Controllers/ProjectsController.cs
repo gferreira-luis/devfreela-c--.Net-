3e257bae14cbceb74755f0480e878f73cd8e0371
Gustavo Luis <gferreiraluis@outlook.com>
2023-10-27 21:27:33 -0300

    init

---
+++ DevFreelaAPI/Controllers/ProjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreelaAPI.Controllers
+{
+    [Route("api/pojects")]
+    public class ProjectsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
